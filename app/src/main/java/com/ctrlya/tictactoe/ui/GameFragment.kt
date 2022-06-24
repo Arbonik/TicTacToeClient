c6d89e68f5029bab0e3506ea758eb0a7c60472ae
arbonik <zmear@mail.ru>
2022-06-25 01:27:52 +0700

    add navigation, koin, application

---
+++ app/src/main/java/com/ctrlya/tictactoe/ui/GameFragment.kt
@@ -0,0 +1,21 @@
+package com.ctrlya.tictactoe.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ctrlya.tictactoe.R
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class GameFragment : Fragment() {
+
+    private val viewModel: GameViewModel by viewModel<GameViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.game_fragment, container, false)
+    }
+}
---
+++ app/src/main/java/com/ctrlya/tictactoe/ui/GameViewModel.kt
@@ -0,0 +1,7 @@
+package com.ctrlya.tictactoe.ui
+
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
