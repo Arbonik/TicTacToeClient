71389e0ab83e445449f77459c535363d9f3f574e
prolapsed-kneecap <2007eia2408@gmail.com>
2022-06-26 16:08:57 +0700

    logo +

--- app/src/main/java/com/ctrlya/tictactoe/MainActivity.kt
+++ app/src/main/java/com/ctrlya/tictactoe/MainActivity.kt
@@ -10,9 +10,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//        window.setFlags(
+//            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//            WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
